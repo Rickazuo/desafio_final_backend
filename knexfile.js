ffdcd757aa6c63dc043c516d277792ce06754190
Rickazuo <rick3209@gmail.com>
2024-02-24 22:25:42 -0300

    update knex

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,13 @@
+const path = require('path');
 module.exports = {
-    development: {
-      client: 'sqlite3',
-      connection: {
-        filename: "./src/database/db.sqlite"
-      },
-      useNullAsDefault: true, 
-      migrations: {
-        directory: __dirname + '/src/database/migrations', 
-      },
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, './src/database/db.sqlite')
     },
-  };
-  
-
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(__dirname, './src/database/migrations')
+    },
+  },
+};
